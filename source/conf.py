e6815b16df84e924193567ed6a3dd4d039f6c292
supperthomas <78900636@qq.com>
2020-09-26 19:39:55 +0800

    add the index

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ author = 'supperthomas'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
-
+master_doc = 'index'
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
